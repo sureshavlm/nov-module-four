a8b1658587e94f103212ee519d0a341a1ac0c9b8
suresh <suresh.avlm@gmail.com>
2018-11-13 09:14:21 +0530

    initial commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'module-four';
+  
+  user:any = {username: "", password: ""};
+  success: Boolean = true;
+
+  products:any = [
+					{name: 'Cico Router', category: 'Network', price: 70},
+					{name: 'Dairy Milk', category: 'Dairy', price: 10},
+					{name: 'Soaps', category: 'Retail', price: 20}
+
+				];
+
+
+	onSubmit(loginForm) {
+		if(loginForm.form.controls.username.value != 'admin' 
+			|| loginForm.form.controls.password.value != 'admin'){
+
+			this.success = false; 
+		}
+
+		alert(this.success);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }   from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { LoggingService } from './log.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
-  providers: [],
+  providers: [ LoggingService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
